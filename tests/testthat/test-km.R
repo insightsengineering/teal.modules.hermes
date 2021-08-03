42dd290be92dad2d32d2e2d8ea73f7d718cf0b8f
Zhang, Lyndsee Midori {MDBN~South San Francisco} <zhang.lyndsee@gene.com>
2021-08-03 13:11:36 -0500

    20 h km mae to adtte (#23)
    * adding prod start
    
    * adding function edits for genes >1
    
    * adding final function edits, working example, and test start
    
    * adding tests
    
    * adding documentation
    
    * Update R/km.R
    
    Co-Authored-By: Sabanes Bove, Daniel {MDBR~Basel} <daniel.sabanes_bove@roche.com>
    
    * Update R/km.R
    
    Co-Authored-By: Sabanes Bove, Daniel {MDBR~Basel} <daniel.sabanes_bove@roche.com>
    
    * adding edits
    
    * adding more edits
    
    * adding last edits
    
    * added params to argument list
    
    * adding last edits
    
    Co-authored-by: Lyndsee Z <zhanl238@gene.com>
    Co-authored-by: Sabanes Bove, Daniel {MDBR~Basel} <daniel.sabanes_bove@roche.com>
    Co-authored-by: Lyndsee Z <zhanl238.com>

---
+++ tests/testthat/test-km.R
@@ -0,0 +1,48 @@
+# h_km_mae_to_adtte ----
+library(random.cdisc.data)
+library(data.table)
+
+test_that("h_km_mae_to_adtte function works as expected with default settings", {
+  mae <- hermes::multi_assay_experiment
+  adtte <- radtte(cached = TRUE) %>% dplyr::mutate(CNSR = as.logical(CNSR))
+  #make sure patient IDs match some in adtte to test function
+  experiment_name <- "se2"
+  se_test <- mae[[experiment_name]]
+  hd_test <- hermes::HermesData(se_test)
+  mae_samplemap <- MultiAssayExperiment::sampleMap(mae)
+  samplemap_experiment <- mae_samplemap[mae_samplemap$assay == experiment_name, ]
+  se_patients <- samplemap_experiment$primary
+  adtte$USUBJID[1:9] <- se_patients
+  gene_var <- c("GeneID:1820", "GeneID:94115")
+  result <- h_km_mae_to_adtte(adtte, mae, gene_var = "GeneID:1820", experiment_name = "se2")
+  result2 <- h_km_mae_to_adtte(adtte, mae, gene_var = gene_var, experiment_name = "se2")
+
+  expect_class(result, "data.frame")
+  expect_class(result2, "data.frame")
+  expect_true("SampleID" %in% colnames(result))
+  expect_true(any(colnames(result) %like% "GeneID"))
+})
+
+test_that("h_km_mae_to_adtte fails as expected with invalid settings", {
+  mae <- hermes::multi_assay_experiment
+  adtte <- radtte(cached = TRUE) %>% dplyr::mutate(CNSR = as.logical(CNSR))
+  #example with no matched patient IDs
+  bad_adtte <- adtte
+
+  #make sure patient IDs match some in adtte to test function
+  experiment_name <- "se2"
+  se_test <- mae[[experiment_name]]
+  hd_test <- hermes::HermesData(se_test)
+  mae_samplemap <- MultiAssayExperiment::sampleMap(mae)
+  samplemap_experiment <- mae_samplemap[mae_samplemap$assay == experiment_name, ]
+  se_patients <- samplemap_experiment$primary
+  adtte$USUBJID[1:9] <- se_patients
+  good_adtte <- adtte
+
+
+  expect_error(h_km_mae_to_adtte(good_adtte, mae, gene_var = "GeneID:1820", experiment_name = "se4"))
+  expect_error(h_km_mae_to_adtte(good_adtte, mae, gene_var = "GeneID:1820", experiment_name = "se2", assay_name = "foo"))
+  expect_error(h_km_mae_to_adtte(good_adtte, mae, gene_var = "GeneID:1820", experiment_name = 1))
+  expect_error(h_km_mae_to_adtte(good_adtte, mae, gene_var = 1520))
+  expect_error(bad_adtte, mae, gene_var = "GeneID:1820")
+})
