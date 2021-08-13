a1748c8e9c5e851f861a86f899457ed4866586f3
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2021-08-13 11:02:48 +0200

    correct tests

--- tests/testthat/test-volcanoplot.R
+++ tests/testthat/test-volcanoplot.R
@@ -18,7 +18,7 @@ test_that("tm_g_volcanoplot works as expected in the sample app", {
   skip_if_covr()
 
   library(shinytest)
-  app <- ShinyDriver$new("tests/testthat/volcanoplot/", loadTimeout = 1e5, debug = "all" )
+  app <- ShinyDriver$new("volcanoplot/", loadTimeout = 1e5, debug = "all", phantomTimeout = 1e5)
   app$getDebugLog()
   app$snapshotInit("test-app")
 
@@ -31,10 +31,10 @@ test_that("tm_g_volcanoplot works as expected in the sample app", {
 
   # Check initial state of encodings.
   initial_experiment_name <- app$waitForValue("teal-main_ui-modules_ui-root_volcanoplot-experiment_name")
-  expect_identical(initial_experiment_name, "se1")
+  expect_identical(initial_experiment_name, "hd1")
 
-  initial_facet_var <- app$waitForValue("teal-main_ui-modules_ui-root_volcanoplot-facet_var")
-  expect_identical(initial_facet_var, "AGE18")
+  initial_compare_group <- app$waitForValue("teal-main_ui-modules_ui-root_volcanoplot-compare_group")
+  expect_identical(initial_compare_group, "AGE18")
 
   # Initial plot.
   expect_snapshot_screenshot(
@@ -43,13 +43,10 @@ test_that("tm_g_volcanoplot works as expected in the sample app", {
     name = "initial_plot.png"
   )
 
-  # Now change the experiment_name and confirm that facet_var is updated accordingly.
+  # Now change the log2_fc_thresh and check that the plot is updated accordingly.
   app$setInputs(
-    "teal-main_ui-modules_ui-root_volcanoplot-experiment_name" = "se3"
+    "teal-main_ui-modules_ui-root_volcanoplot-log2_fc_thresh" = 8
   )
-  now_facet_var <- app$waitForValue("teal-main_ui-modules_ui-root_volcanoplot-facet_var")
-  expect_identical(now_facet_var, "AGE18")
-
 
   # Final plot.
   expect_snapshot_screenshot(
