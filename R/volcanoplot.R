3aab003cf48f1a17fff822b8dc827f4c3a76641b
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2021-08-13 10:41:03 +0200

    some progress

--- R/volcanoplot.R
+++ R/volcanoplot.R
@@ -57,12 +57,13 @@ tm_g_volcanoplot <- function(label,
 #' @describeIn tm_g_volcanoplot sets up the user interface.
 #' @inheritParams module_arguments
 #' @export
-ui_g_volcanoplot <- function(id, datasets, mae_name, pre_output, post_output) {
+ui_g_volcanoplot <- function(id,
+                             datasets,
+                             mae_name,
+                             pre_output,
+                             post_output) {
   ns <- NS(id)
   mae <- datasets$get_data(mae_name, filtered = TRUE)
-  object <- hermes::HermesData(mae[[1]])
-  SummarizedExperiment::colData(object) <- hermes::df_char_to_factor(SummarizedExperiment::colData(object))
-  factor_2L <- names(SummarizedExperiment::colData(object))[apply(SummarizedExperiment::colData(object),2,function(s)nlevels(factor(s))) == 2L]
 
   teal.devel::standard_layout(
     output = div(
@@ -72,8 +73,8 @@ ui_g_volcanoplot <- function(id, datasets, mae_name, pre_output, post_output) {
     post_output = post_output,
     encoding = div(
       selectInput(ns("experiment_name"), "Select experiment", names(mae)),
-      selectInput(ns("compare_groups"), "Compare Groups", choices = factor_2L ),
-      selectInput(ns("method"), "Method", choices = c("voom", "deseq2") ),
+      selectInput(ns("compare_groups"), "Compare Groups", choices = ""),
+      selectInput(ns("method"), "Method", choices = c("voom", "deseq2")),
       sliderInput(ns("log2_fc_thresh"), "Log2 fold change threshold", value = 2.5, min = 0.1, max = 10),
       sliderInput(ns("adj_p_val_thresh"), "Adjusted p-value threshold", value = 0.05, min = 0.001, max = 1),
       tags$label("Show Top Differentiated Genes"),
@@ -85,7 +86,11 @@ ui_g_volcanoplot <- function(id, datasets, mae_name, pre_output, post_output) {
 #' @describeIn tm_g_volcanoplot sets up the server with reactive graph.
 #' @inheritParams module_arguments
 #' @export
-srv_g_volcanoplot <- function(input, output, session, datasets, mae_name) {
+srv_g_volcanoplot <- function(input,
+                              output,
+                              session,
+                              datasets,
+                              mae_name) {
 
   # When the filtered data set of the chosen experiment changes, update the
   # experiment data object.
@@ -99,62 +104,55 @@ srv_g_volcanoplot <- function(input, output, session, datasets, mae_name) {
     object
   })
 
-  # When the chosen experiment changes, recompute the SummarizedExperiment::colData variables.
-  # We only select the SummarizedExperiment::colData variables with 2 levels
-  col_data_vars <- eventReactive(input$experiment_name, ignoreNULL = TRUE, {
+  # When the chosen experiment changes, recompute the group variables.
+  # We only select the colData variables which are factors and have 2 levels.
+  group_vars <- eventReactive(input$experiment_name, ignoreNULL = TRUE, {
     object <- experiment_data()
-    names(SummarizedExperiment::colData(object))[apply(SummarizedExperiment::colData(object),2,function(s)nlevels(factor(s))) == 2L]
+    col_data <- SummarizedExperiment::colData(object)
+    can_be_used <- sapply(col_data, function(x) is.factor(x) && identical(nlevels(x), 2L))
+    names(col_data)[can_be_used]
   })
 
-  # When the SummarizedExperiment::colData variables change, update the choices for compare_groups.
-  observeEvent(col_data_vars(), {
-    facet_fill_var_choices <- col_data_vars()
+  # When the group variables change, update the choices for compare_groups.
+  observeEvent(group_vars(), {
+    group_var_choices <- group_vars()
 
-    id_names <- c("compare_groups")
-    for (i in seq_along(id_names)) {
-      updateSelectInput(
-        session,
-        id_names[i],
-        choices = facet_fill_var_choices,
-        selected = facet_fill_var_choices[1]
-      )
-    }
+    updateSelectInput(
+      session,
+      "compare_groups",
+      choices = group_var_choices,
+      selected = group_var_choices[1]
+    )
   })
 
   # When the filtered data set or the chosen experiment changes, update
-  # the call that creates the Heremes object for differential expression.
+  # the call that creates the Hermes object for differential expression.
   diff_expr <- reactive({
     object <- experiment_data()
-    compare_groups <- input$compare_groups
+    compare_group <- input$compare_group
     method <- input$method
 
-
     req(
-      compare_groups,
-        method,
-      cancelOutput = FALSE
+      object,
+      compare_group,
+      method
     )
 
-
-
-    hermes::diff_expression(object, group = compare_groups, method = method)
+    hermes::diff_expression(object, group = compare_group, method = method)
   })
-
-
-
 
   output$plot <- renderPlot({
     # Resolve all reactivity.
     diff_expr_result <- diff_expr()
     log2_fc_thresh <- input$log2_fc_thresh
     adj_p_val_thresh <- input$adj_p_val_thresh
-    compare_groups <- input$compare_groups
+    compare_group <- input$compare_group
 
     # Require which states need to be truthy.
     req(
       log2_fc_thresh,
       adj_p_val_thresh,
-      compare_groups
+      compare_group
     )
 
 
