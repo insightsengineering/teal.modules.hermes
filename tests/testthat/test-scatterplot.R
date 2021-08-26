8b10768153dcb0bcf6cc4c4f3d2691fe3bf871ef
Junlue Zhao <jzhao31415@gmail.com>
2021-08-26 08:36:46 -0500

    test.nest::skip_if_too_deep(5) to shiny unit tests

--- tests/testthat/test-scatterplot.R
+++ tests/testthat/test-scatterplot.R
@@ -15,6 +15,8 @@ test_that("ui_g_scatterplot creates expected HTML", {
 # tm_g_scatterplot ----
 
 test_that("tm_g_scatterplot works as expected in the sample app", {
+  test.nest::skip_if_too_deep(5)
+
   skip_if_covr()
 
   library(shinytest)
