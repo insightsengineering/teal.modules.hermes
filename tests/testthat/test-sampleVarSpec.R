9a711de42aca2245a79c8a34f3e2c823975192d4
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2021-09-03 18:06:35 +0200

    for test

---
+++ tests/testthat/test-sampleVarSpec.R
@@ -0,0 +1,26 @@
+assign_list <- list(
+  "ASIAN" = "1",
+  "BLACK OR AFRICAN AMERICAN" = "1",
+  "MULTIPLE" = "2",
+  "UNKNOWN" = "2",
+  "WHITE" = "4"
+)
+h_assign_to_group_list(assign_list)
+
+assign_list2 <- list(
+  "ASIAN" = "2",
+  "BLACK OR AFRICAN AMERICAN" = "2",
+  "MULTIPLE" = "1",
+  "UNKNOWN" = "1",
+  "WHITE" = "4"
+)
+h_assign_to_group_list(assign_list2)
+
+assign_list3 <- list(
+  "ASIAN" = "4",
+  "BLACK OR AFRICAN AMERICAN" = "3",
+  "MULTIPLE" = "2",
+  "UNKNOWN" = "1",
+  "WHITE" = "5"
+)
+h_assign_to_group_list(assign_list3)
