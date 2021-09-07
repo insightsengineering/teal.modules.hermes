422c0da628ffe5960aa1c3dca454bbc08156e83f
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2021-09-07 15:47:47 +0200

    start with experimentSpec server module tests

--- tests/testthat/test-experimentSpec.R
+++ tests/testthat/test-experimentSpec.R
@@ -11,3 +11,26 @@ test_that("experimentSpecInput creates expected HTML", {
     label_experiments = "Please select the best experiment"
   ))
 })
+
+# experimentSpecServer ----
+
+test_that("experimentSpec module works as expected in the test app", {
+  skip_if_covr()
+  test.nest::skip_if_too_deep(5)
+
+  library(shinytest)
+  app <- ShinyDriver$new("experimentSpec/", loadTimeout = 1e5, debug = "all", phantomTimeout = 1e5)
+  app$getDebugLog()
+  app$snapshotInit("test-app")
+
+  # nolint start
+
+  ns <- NS("teal-main_ui-modules_ui-root_experimentSpec_example")
+
+  # Initially the first experiment is selected.
+  initial_experiment <- app$waitForValue(ns("my_experiment-name"))
+  expect_identical(initial_experiment, "hd1")
+
+  # The data is correctly processed so we can see the print result.
+
+})
