7332cc64085d219c158988ae024c369e84f5b114
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2021-09-08 14:49:15 +0200

    start design script

---
+++ design/design_gene_names.R
@@ -0,0 +1,85 @@
+library(teal.modules.hermes)
+
+ui <- function(id,
+               datasets) {
+  ns <- NS(id)
+  teal.devel::standard_layout(
+    encoding = div(
+      optionalSelectInput(
+        ns("genes"),
+        label = NULL,
+        choices = "",
+        multiple = TRUE,
+        options = shinyWidgets::pickerOptions(
+          liveSearch = TRUE
+        )
+      )
+    ),
+    output = textOutput(ns("result"))
+  )
+}
+
+h_update_gene_selection2 <- function(session,
+                                    inputId,
+                                    selected,
+                                    choices) {
+  is_new_selected <- selected %in% choices
+  is_removed <- !is_new_selected
+  updateOptionalSelectInput(
+    session,
+    inputId = inputId,
+    selected = selected[is_new_selected],
+    choices = choices
+  )
+  n_removed <- sum(is_removed)
+  if (n_removed > 0) {
+    showNotification(paste(
+      "Removed", n_removed, ifelse(n_removed > 1, "genes", "gene"),
+      hermes::h_parens(hermes::h_short_list(selected[is_removed]))
+    ))
+  }
+}
+
+server <- function(input,
+                   output,
+                   session,
+                   datasets) {
+  gene_choices <- reactive({
+    mae <- datasets$get_data("MAE", filtered = TRUE)
+    rownames(mae[[1]])
+  })
+  observeEvent(gene_choices(), {
+    gene_choices <- gene_choices()
+    old_selected <- input$genes
+
+    h_update_gene_selection2(
+      session,
+      inputId = "genes",
+      selected = old_selected,
+      choices = gene_choices
+    )
+  })
+  output$result <- renderText({
+    input$genes
+  })
+}
+
+my_app <- function() {
+  mae <- hermes::multi_assay_experiment
+  mae_data <- dataset("MAE", mae)
+  data <- teal_data(mae_data)
+  app <- init(
+    data = data,
+    modules = root_modules(
+      module(
+        label = "gene names design",
+        server = server,
+        ui = ui,
+        filters = "all"
+      )
+    )
+  )
+  shinyApp(app$ui, app$server)
+}
+
+my_app()
