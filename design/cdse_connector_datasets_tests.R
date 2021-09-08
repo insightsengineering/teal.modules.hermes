f833acddbb10c56283b28ffd10260056687f804d
namratabhatia <namratab05@gmail.com>
2021-09-08 15:11:40 -0700

    Adding DDL for CDISC examples

--- design/cdse_connector_datasets_tests.R
+++ design/cdse_connector_datasets_tests.R
@@ -10,7 +10,7 @@ data <- cdse_data(
   cdse_dataset_connector(
     dataname = "MAE",
     cid = "cid6828341065561714688"
-  ) #%>% mutate_dataset("MAE[[1]] <- HermesData(MAE[[1]])")
+  ) %>% mutate_dataset("MAE[[1]] <- HermesData(MAE[[1]])")
 )
 
 app <- init(
@@ -25,6 +25,7 @@ shinyApp(app$ui, app$server)
 # Accessing CDSE datasets: https://github.roche.com/sabanesd/nest/blob/master/mae/cdse.R
 
 # # CDISC data set (ADTTE) with existing teal.module.clinical (tm_)
+library(dplyr)
 library(teal)
 library(teal.modules.hermes)
 library(teal.modules.clinical)
@@ -36,19 +37,25 @@ cdse_get_environment()
 cdse_set_environment("prod")
 
 # Using Asthma Data Pilot -- Pooled Integrated Datasets -- ADAM ADSL from https://cdse.roche.com/details/cid6736608260612358144
-dat_single <- cdse_read_dataset("http://clinical.roche.com/dataset/cid6736608260612358144")
+# dat_single <- cdse_read_dataset("http://clinical.roche.com/dataset/cid6736608260612358144")
 
-char_vars_adsl <- names(Filter(isTRUE, sapply(dat_single, is.character)))
+data <- cdse_data(
+  connection = cdse_connection("prod"),
+  cdse_dataset_connector(
+    dataname = "ADSL",
+    cid = "cid6736608260612358144"
+  ) %>% mutate_dataset(
+    code = "ADSL$ACTARMCD <- as.factor(ADSL$ACTARMCD)"
+  )
+)
+
 app <- init(
-  data = cdisc_data(
-    cdisc_dataset("ADSL",
-                  dat_single)
-  ),
+  data = cdisc_data(data),
   root_modules(
     teal.modules.clinical::tm_t_summary(
       label = "Demographic Table",
       dataname = "ADSL",
-      arm_var = choices_selected(c("ACTARM", "ACTARMCD"), "ACTARMCD"),
+      arm_var = choices_selected(c("ACTARMCD"), "ACTARMCD"),
       summarize_vars = choices_selected(c("SAFFL", "DTHFL", "AAGE"), c("SAFFL")),
       add_total = TRUE,
       useNA = c("ifany"),
@@ -63,21 +70,30 @@ app <- init(
 
 shinyApp(app$ui, app$server)
 
-# Using Asthma Data Pilot -- Pooled Integrated Datasets - ADaM ADZB from https://cdse.roche.com/details/cid6736609907921174529
-dat_single <- cdse_read_dataset("http://clinical.roche.com/dataset/cid6736609908885864448")
+# Verify that that the missing values in above dataset are actual missing values, and not an issue with cdse_connector
+library(haven)
+test <- read_sas("/Users/bhatian3/NEST/teal.modules.hermes/design/adsl.sas7bdat")
 
-char_vars_adsl <- names(Filter(isTRUE, sapply(dat_single, is.character)))
+# Using Asthma Data Pilot -- Pooled Integrated Datasets - ADaM ADZB from https://cdse.roche.com/details/cid6736609907921174529
+#dat_single <- cdse_read_dataset("http://clinical.roche.com/dataset/cid6736609908885864448")
+
+data <- cdse_data(
+  connection = cdse_connection("prod"),
+  cdse_dataset_connector(
+    dataname = "ADSL",
+    cid = "cid6736609908885864448"
+  ) %>% mutate_dataset(
+    code = "ADSL$TRT01P <- as.factor(ADSL$TRT01P)"
+  )
+)
 app <- init(
-  data = cdisc_data(
-    cdisc_dataset("ADSL",
-                  dat_single)
-  ),
+  data = cdisc_data(data),
   root_modules(
     teal.modules.clinical::tm_t_summary(
       label = "Demographic Table",
       dataname = "ADSL",
-      arm_var = choices_selected(c("ACTARM", "ACTARMCD", "ARM"), "ACTARMCD"),
-      summarize_vars = choices_selected(c("SAFFL", "AGE"), c("SAFFL")),
+      arm_var = choices_selected(c("TRT01P"), "TRT01P"),
+      summarize_vars = choices_selected(c("SEX", "AGE", "ITTFL"), c("SEX")),
       add_total = TRUE,
       useNA = c("ifany"),
       na_level = "<Missing>",
