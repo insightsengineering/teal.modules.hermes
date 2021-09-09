68a78a92f89828e506e5952d168efb55b79e5d5e
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2021-09-09 21:13:16 +0200

    complete documentation

--- R/assaySpec.R
+++ R/assaySpec.R
@@ -38,7 +38,65 @@ assaySpecInput <- function(inputId,
 #' @export
 #'
 #' @examples
-
+#' ui <- function(id,
+#'                datasets) {
+#'   ns <- NS(id)
+#'   teal.devel::standard_layout(
+#'     encoding = div(
+#'       experimentSpecInput(
+#'         ns("experiment"),
+#'         datasets,
+#'         "MAE"
+#'       ),
+#'       assaySpecInput(
+#'         ns("assay"),
+#'         label_assays = "Please choose assay"
+#'       )
+#'     ),
+#'     output = textOutput(ns("result"))
+#'   )
+#' }
+#'
+#' server <- function(input,
+#'                    output,
+#'                    session,
+#'                    datasets) {
+#'   experiment <- experimentSpecServer(
+#'     "experiment",
+#'     datasets,
+#'     "MAE"
+#'   )
+#'   assay <- assaySpecServer(
+#'     "assay",
+#'     experiment$assays,
+#'     exclude_assays = c("counts", "cpm", "tpm", "bla")
+#'   )
+#'   output$result <- renderPrint({
+#'     assay()
+#'   })
+#' }
+#'
+#' my_app <- function() {
+#'   mae <- hermes::multi_assay_experiment
+#'   mae_name <- "MAE"
+#'   mae_data <- dataset(mae_name, mae)
+#'   data <- teal_data(mae_data)
+#'   app <- init(
+#'     data = data,
+#'     modules = root_modules(
+#'       module(
+#'         label = "assaySpec example",
+#'         server = server,
+#'         ui = ui,
+#'         filters = "all"
+#'       )
+#'     )
+#'   )
+#'   shinyApp(app$ui, app$server)
+#' }
+#' if (interactive()) {
+#'   my_app()
+#' }
 assaySpecServer <- function(inputId,
                             assays,
                             exclude_assays = character()) {
