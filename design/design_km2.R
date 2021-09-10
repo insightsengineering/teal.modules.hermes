ad0a8f5d23af9b08cb6ac65299d6809eb19dd2e5
Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
2021-09-10 10:57:50 +0200

    another lintr fix

--- design/design_km2.R
+++ design/design_km2.R
@@ -8,7 +8,7 @@ gene_var2 <- c("GeneID:1820", "GeneID:94115")
 new_adtte <- h_km_mae_to_adtte(adtte, mae, gene_var = "GeneID:1820", experiment_name = "hd2")
 new_adtte2 <- h_km_mae_to_adtte(adtte, mae, gene_var = gene_var2, experiment_name = "hd2")
 
-ANL <- new_adtte
+ANL <- new_adtte # nolint
 names(ANL)
 arm_var <- attr(ANL, "gene_cols")
 mycode <- template_g_km_mae(arm_var = arm_var)
