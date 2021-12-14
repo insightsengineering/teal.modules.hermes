c9d90fc05302d055c3e30c9cf8478acc5e250fc3
Dawid Kałędkowski <dawid.kaledkowski@contractors.roche.com>
2021-12-14 10:17:44 +0100

    last .lint

--- tests/testthat/test-quality.R
+++ tests/testthat/test-quality.R
@@ -20,7 +20,8 @@ test_that("tm_g_quality works as expected in the sample app", {
   skip_if_covr()
 
   library(shinytest)
-  app <- ShinyDriver$new(testthat::test_path("quality"), loadTimeout = 1e5, debug = "all", phantomTimeout = 1e5, seed = 123)
+  app <- ShinyDriver$new(testthat::test_path("quality"), loadTimeout = 1e5,
+                         debug = "all", phantomTimeout = 1e5, seed = 123)
   on.exit(app$stop())
   app$getDebugLog()
   app$snapshotInit("test-app")
