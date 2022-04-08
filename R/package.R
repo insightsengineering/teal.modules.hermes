1e758b88cbf4e2ab447cdde1c90c01d9f4a35216
Dawid Kałędkowski <dawid.kaledkowski@gmail.com>
2022-04-08 11:48:25 +0200

    vbump [skip vbump]

--- R/package.R
+++ R/package.R
@@ -8,7 +8,6 @@
 #' @import ggplot2
 #' @import shiny
 #' @import teal
-#' @importFrom teal.code init_chunks chunks_reset chunks_push chunks_safe_eval
 #' @importFrom S4Vectors setListElement DataFrame
 #' @importFrom lifecycle deprecate_soft
 NULL
