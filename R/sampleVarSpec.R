fc0b9f8f439016a79e6c6e6c815b41750723417c
Nikolas Burkoff <nikolas.burkoff@roches.com>
2022-06-09 12:19:40 +0100

    add length check

--- R/sampleVarSpec.R
+++ R/sampleVarSpec.R
@@ -402,7 +402,8 @@ sampleVarSpecServer <- function(id,
 
           old_values <- names(assign_lists[[experiment_name]][[sample_var]])
           if (!is.null(old_values) &&
-            all(sort(unique(old_values)) == sort(sample_var_levels))) {
+              length(old_values) == length(sample_var_levels) &&
+              all(sort(unique(old_values)) == sort(sample_var_levels))) {
             selected_groups <- assign_lists[[experiment_name]][[sample_var]]
           }
 
