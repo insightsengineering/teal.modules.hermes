05ea34df25ca4e7374e50ae2edd036ee2e25e204
benoit <benoit.falquet@roche.com>
2022-10-17 09:32:45 +0200

    assaySpec

---
+++ tests/testthat/helper-shinytest2.R
@@ -0,0 +1,8 @@
+module_ns_shiny2 <- function(app) {
+  source <- app$get_html("html", outer_html = TRUE)
+  module_id <- rvest::html_attr(
+    rvest::html_node(rvest::read_html(source), css = ".teal_module"),
+    "id"
+  )
+  NS(module_id)
+}
---
+++ tests/testthat/test-shinytest2-assaySpec.R
@@ -0,0 +1,40 @@
+library(shinytest2)
+
+test_that("{shinytest2} recording: assaySpecServer module works as expected in the test app", {
+  app <- AppDriver$new(
+    app_dir = "assaySpec",
+    name = "assaySpec module works as expected in the test app",
+    height = 520, width = 979
+  )
+  ns <- module_ns_shiny2(app)
+  ns2 <- NS("teal-main_ui-filter_panel")
+
+  # Validation message because no assays eligible in first experiment.
+  app$wait_for_idle()
+  res <- app$wait_for_value(output = ns("result"))
+  expect_identical(res$message, "No assays eligible for this experiment, please make sure to add normalized assays")
+
+  # Select the second experiment and see that we can select the right assays.
+  app$set_inputs(!!ns("experiment-name") := "hd2")
+  app$wait_for_idle()
+
+  res <- app$get_values()
+  print(res)
+
+  res <- app$get_value(input = ns("assay-name"))
+  expect_identical(res, "rpkm")
+
+  res <- app$get_value(output = ns("result"))
+  expect_identical(res, "[1] \"rpkm\"")
+
+  app$set_inputs(!!ns("assay-name") := "voom")
+  app$wait_for_idle()
+  res <- app$get_value(output = ns("result"))
+  expect_identical(res, "[1] \"voom\"")
+
+  # Check that cpm should not be available.
+  app$set_inputs(!!ns("assay-name") := "cpm")
+  app$wait_for_idle()
+  res <- app$get_value(output = ns("result"))
+  expect_identical(res, "[1] \"\"")
+})
