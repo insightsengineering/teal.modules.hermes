3d8b64e9b91aac0ca163c257ed03938f6bf3de64
benoit <benoit.falquet@roche.com>
2022-10-17 11:53:35 +0200

    geneSpec test

---
+++ tests/testthat/test-shinytest2-geneSpec.R
@@ -0,0 +1,81 @@
+library(shinytest2)
+
+test_that("geneSpec module works as expected in the test app", {
+  app <- AppDriver$new(
+    app_dir = "geneSpec",
+    name = "geneSpec module works as expected in the test app"
+  )
+  ns <- module_ns_shiny2(app)
+  ns2 <- NS("teal-main_ui-filter_panel")
+
+  app$wait_for_idle()
+
+  res <- app$get_value(input = ns("my_genes"))
+  expect_null(res)
+
+  res <- app$get_value(output = ns("result"))
+  expect_identical(res$message, "please select at least one gene")
+
+  # Set genes manually.
+  selected_genes <- c(
+    "GeneID:10061", "GeneID:28", "GeneID:47", "GeneID:8310",
+    "GeneID:52", "GeneID:88", "GeneID:11096"
+  )
+
+  app$set_inputs(!!ns("my_genes-genes") := selected_genes)
+  app$wait_for_idle()
+
+  # See that now the first function is selected.
+  res <- app$get_value(input = ns("my_genes-fun_name"))
+  expect_identical(res, "mean")
+
+  # Get the expected result.
+  res <- app$get_value(output = ns("result"))
+  expect_identical(res, "mean(ABCF2, ABO, ..., ADAMTS5)")
+
+  # Add chromosome filters for the first experiment.
+  app$set_inputs(!!ns2("add_MAE_filter-hd1-row_to_add") := "chromosome")
+
+  # Lock the gene selection.
+  app$set_inputs(!!ns("my_genes-lock_button") := TRUE)
+  app$wait_for_idle()
+  app$set_inputs(!!ns2("MAE_filter-hd1-rowData_var_chromosome-content-selection") := c("1", "2"))
+  app$wait_for_idle()
+
+  # Confirm that gene selection was not changed.
+  # Note: Due to sorting by gene name the order might not be the same.
+  res <- app$get_value(input = ns("my_genes-genes"))
+  expect_set_equal(res, selected_genes)
+
+  res <- app$get_value(output = ns("result"))
+  expect_identical(
+    res$message,
+    "5 genes (GeneID:10061, GeneID:28, ..., GeneID:11096) not included, please unlock or change filters"
+  )
+
+  # Unlock the gene selection.
+  app$set_inputs(!!ns("my_genes-lock_button") := FALSE)
+  app$wait_for_idle()
+
+  # Check that gene selection was reduced accordingly.
+  res <- app$get_value(input = ns("my_genes-genes"))
+  expect_set_equal(res, c("GeneID:52", "GeneID:88"))
+  expect_subset(res, selected_genes)
+  expect_length(setdiff(selected_genes, res), 5)
+
+  # Get the expected result.
+  res <- app$get_value(output = ns("result"))
+  expect_identical(res, "mean(ACP1, ACTN2)")
+
+  # Remove the filter.
+  app$click(ns2("MAE_filter-hd1-rowData_var_chromosome-remove"))
+
+  # Select a gene via text input.
+  app$click(ns("my_genes-text_button"))
+  app$wait_for_idle()
+  app$set_inputs(!!ns("my_genes-gene_text") := "GeneID:10061; GeneID:28")
+  app$click(ns("my_genes-ok_button"))
+  app$wait_for_idle()
+  res <- app$get_value(input = ns("my_genes-genes"))
+  expect_set_equal(res, c("GeneID:10061", "GeneID:28"))
+})
