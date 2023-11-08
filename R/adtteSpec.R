6c2ed190f82e53218cfc3645acd6777c1282b2ef
m7pr <marcin.kosinski.mk1@roche.com>
2023-11-08 11:54:25 +0100

    fix one more example

--- R/adtteSpec.R
+++ R/adtteSpec.R
@@ -208,18 +208,15 @@ adtteSpecInput <- function(inputId, # nolint
 #' }
 #'
 #' my_app <- function() {
-#'   mae <- hermes::multi_assay_experiment
 #'   adtte <- teal.modules.hermes::rADTTE %>%
 #'     dplyr::mutate(is_event = .data$CNSR == 0)
 #'
 #'   data <- teal_data(
-#'     dataset(
-#'       "ADTTE",
-#'       adtte,
-#'       code = "adtte <- teal.modules.hermes::rADTTE
+#'     ADTTE = adtte,
+#'     MAE = hermes::multi_assay_experiment,
+#'     code =
+#'        "adtte <- teal.modules.hermes::rADTTE
 #'         dplyr::mutate(is_event = .data$CNSR == 0)"
-#'     ),
-#'     dataset("MAE", mae)
 #'   )
 #'
 #'   app <- init(
