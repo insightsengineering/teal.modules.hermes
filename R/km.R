b73e48af8d1d45b0fae23e93727e0d490af54473
Pawel Rucki <12943682+pawelru@users.noreply.github.com>
2024-02-01 16:18:11 +0100

    register teal.logger handlers

--- R/km.R
+++ R/km.R
@@ -57,7 +57,7 @@ tm_g_km <- function(label,
                     ),
                     pre_output = NULL,
                     post_output = NULL) {
-  logger::log_info("Initializing tm_g_km")
+  message("Initializing tm_g_km")
   assert_string(label)
   assert_string(adtte_name)
   assert_string(mae_name)
