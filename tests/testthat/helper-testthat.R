b6292e99ce6f9e65ebd599dedafcb3bd9142a533
m7pr <marcin.kosinski.mk1@roche.com>
2025-06-25 09:29:19 +0200

    skip covr for noSuggests

--- tests/testthat/helper-testthat.R
+++ tests/testthat/helper-testthat.R
@@ -1,8 +1,12 @@
 # Shortcut to skip a shinytest when coverage is running.
 skip_if_covr <- function() {
-  covr_is_running <- covr::in_covr()
-  skip_if(
-    covr_is_running,
-    message = "not running shinytest while covr is running"
-  )
+  if (!requireNamespace("covr", quietly = TRUE)) {
+    testthat::skip("covr not installed")
+  } else {
+    covr_is_running <- covr::in_covr()
+    skip_if(
+      covr_is_running,
+      message = "not running shinytest while covr is running"
+    )
+  }
 }
